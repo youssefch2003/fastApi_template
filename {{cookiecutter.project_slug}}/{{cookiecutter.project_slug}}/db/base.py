fff39cb0fa58fb1cbf7454e271bdf1cf47f62471
youssefch2003 <medyoussef429@gmail.com>
2024-07-11 12:43:04 +0100

    FInal

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/db/base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/db/base.py
@@ -1,32 +1,32 @@
-# from datetime import datetime
-# from typing import TypeVar
+from datetime import datetime
+from typing import TypeVar
 
-# from sqlalchemy import Column, Integer, MetaData, DateTime
-# from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, MetaData, DateTime
+from sqlalchemy.orm import declarative_base
 
-# __all__ = ("Base",)
-
-
-# meta = MetaData(
-#     naming_convention={
-#         "ix": "ix_%(column_0_label)s",
-#         "uq": "uq_%(table_name)s_%(column_0_name)s",
-#         "ck": "ck_%(table_name)s_`%(constraint_name)s`",
-#         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-#         "pk": "pk_%(table_name)s",
-#     }
-# )
+__all__ = ("Base",)
 
 
-# class _Base:
-#     """Base class for all database models."""
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
 
-#     id = Column(Integer, primary_key=True, index=True)
 
-#     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-#     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+class _Base:
+    """Base class for all database models."""
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
 
-# Base = declarative_base(cls=_Base, metadata=meta)
+Base = declarative_base(cls=_Base, metadata=meta)
 
-# ConcreteTable = TypeVar("ConcreteTable", bound=Base)
+ConcreteTable = TypeVar("ConcreteTable", bound=Base)
